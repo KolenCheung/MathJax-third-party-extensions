eca757616e6ac505abf6e06d74670ffcaf893d27
KolenCheung <christian.kolen@gmail.com>
2016-04-12 04:31:04 -0700

    Removing the tab character in the JSs

--- physics/physics.sh
+++ physics/physics.sh
@@ -3,10 +3,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 # uglify & beautify
+sed -i '' s/'\	'// test/macro.js
 uglifyjs test/macro.js -b -o test/macro.js
 cat License.js test/macro.js > temp.js
 mv temp.js test/macro.js
 
+sed -i '' s/'\	'// unpacked/physics.js
 uglifyjs unpacked/physics.js -b -o unpacked/physics.js
 cat License.js unpacked/physics.js > temp.js
 mv temp.js unpacked/physics.js
@@ -19,10 +21,9 @@ sed -i '' s/"unpacked\/"// physics.js
 # MultiMarkdown
 
 mmd test/macro.md
-mmd README.md
-mv README.html index.html
+mmd index.md
 
-cp README.md temp.md
+cp index.md temp.md
 mmd2pdf temp.md
 mv temp.pdf index.pdf
 find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
